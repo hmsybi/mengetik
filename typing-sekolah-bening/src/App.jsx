b8e1ea14f2a6b84c945f9d8697114242d8502bc5
hmsybi <humas.ybi@gmail.com>
2026-04-26 22:08:34 +0700

    Delete typing-sekolah-bening directory

--- typing-sekolah-bening/src/App.jsx
+++
@@ -1,6 +0,0 @@
-
-import React,{useState} from 'react';
-export default function App(){
- const [msg]=useState('Project siap upload. Tempel App versi lengkap dari canvas jika ingin fitur penuh.');
- return <div><h1>Typing Sekolah Bening</h1><p>{msg}</p></div>
-}
