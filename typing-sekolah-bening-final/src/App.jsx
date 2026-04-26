954d9ef18475936ef22bae86138eb7a6d32474a2
hmsybi <humas.ybi@gmail.com>
2026-04-26 22:21:45 +0700

    Delete typing-sekolah-bening-final directory

--- typing-sekolah-bening-final/src/App.jsx
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-export default function App(){return <div style={{fontFamily:'Arial',padding:20}}><h1>Typing Sekolah Bening Final</h1><p>ZIP Final siap upload ke Vercel/GitHub.</p></div>}
